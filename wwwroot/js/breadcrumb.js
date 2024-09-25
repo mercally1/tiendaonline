3fc6c73d8ef2a708d8dff21708d36a2fe1d05c5b
mercally1 <31991854+mercally1@users.noreply.github.com>
2024-09-24 23:26:09 -0600

    Fix  code file javascript

--- wwwroot/js/breadcrumb.js
+++ wwwroot/js/breadcrumb.js
@@ -2,7 +2,7 @@ const breadcrumbContainer =document.getElementById("breadcrumb-container")
 
 function updateBreadcrumb(){
     breadcrumbContainer.innerHTML= "";
-    breadcrumbContainer.innerHTML+='<li class = "  breadcrumb-item"><a href="/">Home</a></li>';
+    breadcrumbContainer.innerHTML+='<li class = "breadcrumb-item"><a href="/">Home</a></li>';
 
     if (controllerName !== "Home"){
         breadcrumbContainer.innerHTML+=
