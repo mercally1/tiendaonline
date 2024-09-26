d83483790c798a2902c76198bbfd4301c82bc8d9
mercally1 <31991854+mercally1@users.noreply.github.com>
2024-09-26 00:22:45 -0600

    Building views of controller access account

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,147 @@
+﻿using System.Data.Common;
+using System.Security.Claims;
+using System.Security.Principal;
+using IdentityModel;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using tienda.Data;
+using tienda.Models;
+
+namespace tienda.Controllers
+{
+    public class AccountController : BaseController
+    {
+        public AccountController(OnlineShopDbContext context) : base(context)
+        {
+
+        }
+
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> Register(Usuario usuario)
+        {
+            try
+            {
+                if (usuario != null)
+                {
+                    if (await _context.usuarios.AnyAsync(u => u.NombreUsuario == usuario.NombreUsuario))
+                    {
+                        ModelState.AddModelError(nameof(usuario.Nombre), "El nombre de usuario ya esta en uso");
+                        return View(usuario);
+                    }
+
+                    var clienteRol = await _context.roles.FirstOrDefaultAsync(r => r.Nombre=="Cliente");
+
+                    if (clienteRol != null)
+                    {
+                        usuario.RolId = clienteRol.RolId;
+                    }
+
+                    usuario.Direcciones = new List<Direccion>()
+                    {
+                        new Direccion
+                        {
+                            Address= usuario.Direccion,
+                            Cuidad = usuario.Cuidad,
+                            Departamento = usuario.Departamento,
+                            CodigoPostal = usuario.CodigoPostal
+                        }
+                    };
+
+                    _context.usuarios.Add(usuario);
+                    await _context.SaveChangesAsync();
+
+                    var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
+                    identity.AddClaim(new Claim(ClaimTypes.Name, usuario.NombreUsuario));
+                    identity.AddClaim(new Claim(ClaimTypes.Role, "Cliente"));
+
+                    await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(identity));
+
+                    return RedirectToAction("Index", "Home");
+                }
+                return View(usuario);  
+            }
+            catch (DbException dbException)
+            {
+                return HandleDbError(dbException);
+            }
+
+            // return View();
+        }
+    
+        public ActionResult Login()
+        {
+            if(User.Identity!=null && User.Identity.IsAuthenticated)
+            {
+                if (User.IsInRole("Administrator") || User.IsInRole("staff"))
+                    return RedirectToAction("Index", "Home");
+                else
+                    return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<ActionResult> Login(string username, string password)
+        {
+            try
+            {
+                var user = await _context.usuarios.FirstOrDefaultAsync(u=>u.NombreUsuario==username && u.Contrasenia==password); 
+                
+                if (user != null)
+                {
+                    var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
+                    identity.AddClaim(new Claim(ClaimTypes.Name, username));
+                    identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, user.UsuarioId.ToString()));
+
+                    var rol = await _context.roles.FirstOrDefaultAsync(r=>r.RolId == user.RolId);
+                    
+                    if (rol!=null)
+                    {
+                       identity.AddClaim(new Claim(ClaimTypes.Role, rol.Nombre));
+                    }
+                    
+                    await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(identity));
+
+                    if (User.IsInRole("Administrator") || User.IsInRole("staff"))
+                        return RedirectToAction("Index", "Dashboard ");
+                    else
+                        return RedirectToAction("Index", "Home");
+                }
+               
+                ModelState.AddModelError("", "Credenciales Invalidas.");
+                return View();
+            }
+            catch (Exception e)
+            {
+                return (ActionResult)HandleError(e);
+            }
+        }
+
+        public async Task<ActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login");
+        }
+
+        [AllowAnonymous]
+        public ActionResult AccessDenied()
+        {
+            return View();
+        }
+
+    }
+}
+
+
+
