b535d8a96b1f36e736522c312ab7914afec6b4e4
mercally1 <31991854+mercally1@users.noreply.github.com>
2024-09-27 00:12:05 -0600

    Building the views and controlador perfil

---
+++ Controllers/PerfilController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using tienda.Data;
+using tienda.Models;
+
+namespace tienda.Controllers
+{
+    public class PerfilController : BaseController
+    {
+        public PerfilController (OnlineShopDbContext context) : base (context)
+        { }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            return NotFound();
+
+            var usuario =await _context.usuarios
+            .Include(u => u.Direcciones)
+            .FirstOrDefaultAsync(u => u.UsuarioId == id);
+
+            if (usuario == null)
+            return NotFound();
+
+            return View(usuario);
+        }
+
+        public IActionResult AgregarDireccion(int id)
+        {
+            ViewBag.id = id;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AgregarDireccion(Direccion direccion,int id)
+        {
+            try
+            {
+                var usuario = await _context.usuarios.FirstOrDefaultAsync(u => u.UsuarioId==id);
+                if (usuario != null)
+                direccion.Usuarios = usuario;
+
+                _context.Add(direccion);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Details", new {id}); 
+            }
+            catch (System.Exception)
+            {
+                return View(direccion);
+            }
+        }
+    }
+}
